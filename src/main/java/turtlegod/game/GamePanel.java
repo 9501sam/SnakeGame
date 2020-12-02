6c902c003f7abde9f0b47231ad55a31d2a706242
turtlegod <9501sam@gmail.com>
2020-12-03 02:08:33 +0800

    modify pom.xml, create class: GameFrame GamePanel SnakeGame
    pom.xml: set jre to javaSE-13
    SnakeGame, GameFrame, GamePanel: show a foo window

---
+++ src/main/java/turtlegod/game/GamePanel.java
@@ -0,0 +1,15 @@
+package turtlegod.game;
+
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel(){
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
